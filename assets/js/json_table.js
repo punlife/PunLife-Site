ba9aa3220bf7ebb20aaf0b1e4c8ab006107b6dec
PunLife <lolkares@gmail.com>
2026-03-22 13:55:54 +0000

    Revamped experience table + added all events

--- assets/js/json_table.js
+++ assets/js/json_table.js
@@ -1,5 +1,6 @@
 function LoadCommentaryExperience(filepath, tableclass){
 	$.getJSON(filepath, function(jsonData) {
+		let dataset = [];
 		let col = [];
 
 		for (let i = 0; i < jsonData.length; i++) {
@@ -10,39 +11,39 @@ function LoadCommentaryExperience(filepath, tableclass){
 			}  
 		}
 
-		const table = document.createElement("table");
-		const thead = table.createTHead();
-		const tbody = table.createTBody();
-
-		table.setAttribute("id", "json-table");
-		
-		let tr = thead.insertRow(-1);
-
-		for (let index = 0; index < col.length; index++) {
-			let th = document.createElement("th");
-			th.innerHTML = col[index];
-			tr.appendChild(th)
-		}
-
-
 		for (let i = 0; i < jsonData.length; i++) {
-			tr = tbody.insertRow(-1);
-
+			let row = [];
 			for (let j = 0; j < col.length; j++) {
-				let tabCell = tr.insertCell(-1);
-				tabCell.innerHTML = jsonData[i][col[j]] /* jsonData[0]["userId"]*/
+				row.push(jsonData[i][col[j]]) /* jsonData[0]["userId"]*/
 			}
+			dataset.push(row)
 			
 		}
+
+		let table = new DataTable('#experience_table', {
+			columns: [
+				{ title: 'Name' },
+				{ title: 'Date' },
+				{ title: 'Type', orderable: false },
+				{ title: 'Location' },
+				{ title: 'Game' },
+				{ title: 'Notes', orderable: false },
+			],
+			columnDefs: [
+			{
+				targets: [0,1,2,3,4,5],
+				className: 'dt-center'
+			}],
+			data: dataset,
+			lengthChange: false,
+			pageLength: 25,
+			order: [[1, 'dsc']]
+		});
+
 		document.querySelector(tableclass).appendChild(table);
 	});
 	
 }
 
-function CheckIfColumnShouldBeVisible(){
-	
-}
 
-LoadCommentaryExperience('data/tekken8_commentary_experience.json', '.tekken_eight_table');
-LoadCommentaryExperience('data/tekken7_commentary_experience.json', '.tekken_seven_table');
-LoadCommentaryExperience('data/others_commentary_experience.json', '.other_game_table');
+LoadCommentaryExperience('data/commentary_experience.json', '.experience_table');
